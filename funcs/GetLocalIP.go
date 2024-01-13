f5901813bf7360bcf01f9265112ac3bc2c971a54
amali <e.amjad2010@gmail.com>
2024-01-13 17:17:51 +0300

    add commints getlocalIP

--- funcs/GetLocalIP.go
+++ funcs/GetLocalIP.go
@@ -5,14 +5,19 @@ import (
 	"net"
 )
 
+// GetLocalIP retrieves the local IP address by establishing a UDP connection to a known IP address (e.g., Google's DNS server).
+// It returns the local IP address as a string.
 func GetLocalIP() string {
+	// Establish a UDP connection to a known IP address (e.g., Google's DNS server)
 	conn, err := net.Dial("udp", "8.8.8.8:80")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer conn.Close()
 
+	// Retrieve the local address information from the connection
 	localAddress := conn.LocalAddr().(*net.UDPAddr)
 
+	// Return the local IP address as a string
 	return localAddress.IP.String()
 }
